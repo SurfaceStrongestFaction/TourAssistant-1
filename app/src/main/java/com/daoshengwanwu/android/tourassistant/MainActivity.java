ca85aaed2219a1ba1c410d408ba1ca460b78490e
白浩然 <Bai_Haoran@outlook.com>
2016-11-21 15:10:54 +0800

    白浩然提交第二行注释

--- app/src/main/java/com/daoshengwanwu/android/tourassistant/MainActivity.java
+++ app/src/main/java/com/daoshengwanwu/android/tourassistant/MainActivity.java
@@ -2,6 +2,7 @@ package com.daoshengwanwu.android.tourassistant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 

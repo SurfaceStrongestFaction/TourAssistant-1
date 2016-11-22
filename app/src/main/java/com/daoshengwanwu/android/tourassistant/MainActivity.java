e7ac170d208b3dfbf79b7b1c520aa3241c158814
caizihuan <2511480600@qq.com>
2016-11-22 12:35:30 +0800

    蔡自欢

--- app/src/main/java/com/daoshengwanwu/android/tourassistant/MainActivity.java
+++ app/src/main/java/com/daoshengwanwu/android/tourassistant/MainActivity.java
@@ -1,10 +1,9 @@
 package com.daoshengwanwu.android.tourassistant;
 
-<<<<<<< HEAD
 import android.app.Activity;
-=======
+
+import android.app.Activity;
 import android.support.v7.app.ActionBar;
->>>>>>> origin/master
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -15,13 +14,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-=======
 
         ActionBar actionBar = getSupportActionBar();//删除标题栏
         if (null != actionBar) {
             actionBar.hide();
         }
->>>>>>> origin/master
     }
 }

2be7cd0e8f8d2e456cd59ba623dbb6d0d4b3511c
caizihuan <2511480600@qq.com>
2016-11-22 10:56:29 +0800

    caizihuan

--- app/src/main/java/com/daoshengwanwu/android/tourassistant/SearchMemberActivity.java
+++ app/src/main/java/com/daoshengwanwu/android/tourassistant/SearchMemberActivity.java
@@ -3,13 +3,12 @@ package com.daoshengwanwu.android.tourassistant;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class SearchMemberActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search_member);
     }
 }
